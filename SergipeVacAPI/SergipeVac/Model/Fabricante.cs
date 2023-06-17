1014f68e345ec028ae4f26f09bf10cc8d4c030d0
Vinicius Cunha <vinicius.git36@gmail.com>
2023-06-17 18:14:59 -0300

    Exclusion MINIMAL API

--- SergipeVacAPI/SergipeVac/Model/Fabricante.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace SergipeVac.Model
-{
-    public class Fabricante
-    {
-        public int Id { get; set; }
-        public int Referencia { get; set; }
-        public int Nome { get; set;}
-    }
-}
