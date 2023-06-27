51fe4688f5ba89a0d726f9ae959fb6c91ae52c57
Vinicius Cunha <vinicius.git36@gmail.com>
2023-06-27 00:19:03 -0300

    EndPoint de pessoas por etnia vacinadas

--- SergipeVac/SergipeVac/Controllers/RelatorioController.cs
+++ SergipeVac/SergipeVac/Controllers/RelatorioController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using SergipeVac.Model;
+using SergipeVac.Model.Interface;
 
 namespace SergipeVac.Controllers
 {
@@ -7,19 +9,42 @@ namespace SergipeVac.Controllers
     [Route("api/[controller]")]
     public class RelatorioController : Controller
     {
-        [HttpGet]
-        public async Task<JsonResult> ObterContagemPorEtnia()
-        {
-            var dados = new
-            {
-                amarelo = 190,
-                pardo = 111,
-                preto = 10,
-                branco = 1,
-            };
+        private readonly IRepositorio<DocumentoImportadoCSV> _repositorio;
 
-            return Json(dados);
-            
+        public RelatorioController(IRepositorio<DocumentoImportadoCSV> repositorio)
+        {
+            _repositorio = repositorio;
+        }
+
+        //[HttpGet(Name = "ObterContagemPorEtnia")]
+        [HttpGet]
+        public async Task<JsonResult> ObterContagemPorEtnia(DateTime? dataMin = null, DateTime? dataMax = null)
+        {
+            if (!dataMin.HasValue)
+            {
+                dataMin = DateTime.MinValue;
+            }
+            if (!dataMax.HasValue)
+            {
+                dataMax = DateTime.MaxValue;
+            }
+
+            var dataMinUtc = new DateTimeOffset(dataMin.Value, TimeSpan.Zero);
+            var dataMaxUtc = new DateTimeOffset(dataMax.Value, TimeSpan.Zero);
+
+            var documentos = _repositorio.Obter(p => p.VacinaDataAplicacao >= dataMinUtc && p.VacinaDataAplicacao <= dataMaxUtc);
+
+
+            var vacinasPorEtnia = documentos
+                .GroupBy(d => d.PacienteRacaCorValor)
+                .Select(g => new
+                {
+                    PacienteRacaCorValor = g.Key,
+                    TotalPacientes = g.Select(d => d.PacienteId).Distinct().Count()
+                })
+                .ToList();
+
+            return Json(vacinasPorEtnia);
         }
     }
 }
