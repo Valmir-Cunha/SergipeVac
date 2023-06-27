fe2d38a65d6fa57e92025e4764b257c96c9f96d6
Gustavo Caetano Santos <73549261+Gustavo-caetano@users.noreply.github.com>
2023-06-26 22:46:25 -0300

    primeira rota

---
+++ SergipeVac/SergipeVac/Controllers/RelatorioController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SergipeVac.Controllers
+{
+
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RelatorioController : Controller
+    {
+        [HttpGet]
+        public async Task<JsonResult> ObterContagemPorEtnia()
+        {
+            var dados = new
+            {
+                amarelo = 190,
+                pardo = 111,
+                preto = 10,
+                branco = 1,
+            };
+
+            return Json(dados);
+            
+        }
+    }
+}
