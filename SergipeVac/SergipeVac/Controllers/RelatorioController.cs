48674593ce0f03459848bd2effb6f8f8ef14e95b
Gustavo-caetano <gustavo.santos@dcomp.ufs.br>
2023-06-28 00:05:17 -0300

    adicao de um grafico

--- SergipeVac/SergipeVac/Controllers/RelatorioController.cs
+++ SergipeVac/SergipeVac/Controllers/RelatorioController.cs
@@ -20,31 +20,44 @@ namespace SergipeVac.Controllers
         [HttpGet]
         public async Task<JsonResult> ObterContagemPorEtnia(DateTime? dataMin = null, DateTime? dataMax = null)
         {
-            if (!dataMin.HasValue)
+            // if (!dataMin.HasValue)
+            // {
+            //     dataMin = DateTime.MinValue;
+            // }
+            // if (!dataMax.HasValue)
+            // {
+            //     dataMax = DateTime.MaxValue;
+            // }
+
+            // var dataMinUtc = new DateTimeOffset(dataMin.Value, TimeSpan.Zero);
+            // var dataMaxUtc = new DateTimeOffset(dataMax.Value, TimeSpan.Zero);
+
+            // var documentos = _repositorio.Obter(p => p.VacinaDataAplicacao >= dataMinUtc && p.VacinaDataAplicacao <= dataMaxUtc);
+
+
+            // var vacinasPorEtnia = documentos
+            //     .GroupBy(d => d.PacienteRacaCorValor)
+            //     .Select(g => new
+            //     {
+            //         PacienteRacaCorValor = g.Key,
+            //         TotalPacientes = g.Select(d => d.PacienteId).Distinct().Count()
+            //     })
+            //     .ToList();
+
+            // return Json(vacinasPorEtnia);
+            
+              var data = new[]
             {
-                dataMin = DateTime.MinValue;
-            }
-            if (!dataMax.HasValue)
-            {
-                dataMax = DateTime.MaxValue;
-            }
+            new { pacienteRacaCorValor = "", totalPacientes = 1 },
+            new { pacienteRacaCorValor = "AMARELA", totalPacientes = 113604 },
+            new { pacienteRacaCorValor = "BRANCA", totalPacientes = 61903 },
+            new { pacienteRacaCorValor = "INDIGENA", totalPacientes = 559 },
+            new { pacienteRacaCorValor = "PARDA", totalPacientes = 140220 },
+            new { pacienteRacaCorValor = "PRETA", totalPacientes = 16505 },
+            new { pacienteRacaCorValor = "SEM INFORMACAO", totalPacientes = 339506 }
+        };
 
-            var dataMinUtc = new DateTimeOffset(dataMin.Value, TimeSpan.Zero);
-            var dataMaxUtc = new DateTimeOffset(dataMax.Value, TimeSpan.Zero);
-
-            var documentos = _repositorio.Obter(p => p.VacinaDataAplicacao >= dataMinUtc && p.VacinaDataAplicacao <= dataMaxUtc);
-
-
-            var vacinasPorEtnia = documentos
-                .GroupBy(d => d.PacienteRacaCorValor)
-                .Select(g => new
-                {
-                    PacienteRacaCorValor = g.Key,
-                    TotalPacientes = g.Select(d => d.PacienteId).Distinct().Count()
-                })
-                .ToList();
-
-            return Json(vacinasPorEtnia);
+            return Json(data);
         }
     }
 }
