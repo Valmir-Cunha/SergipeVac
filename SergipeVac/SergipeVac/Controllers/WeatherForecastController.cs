ae14851291d8263b9a8537410459adefaef31ed0
Vinicius Cunha <vinicius.git36@gmail.com>
2023-06-28 00:46:46 -0300

    Criado endPoint para as 10 perguntas

--- SergipeVac/SergipeVac/Controllers/WeatherForecastController.cs
+++ SergipeVac/SergipeVac/Controllers/WeatherForecastController.cs
@@ -7,11 +7,6 @@ namespace SergipeVac.Controllers
     [Route("[controller]")]
     public class WeatherForecastController : ControllerBase
     {
-        private static readonly string[] Summaries = new[]
-        {
-        "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
         private readonly ILogger<WeatherForecastController> _logger;
         private readonly ConversorDados _conversorDados;
 
